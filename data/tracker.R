8a8d14ffb024090c08c5fc36fb2c72be2504fd00
zambujo <zambuj@gmail.com>
2022-04-26 20:21:52 +0000

    Updated tracker from rstudio.cloud

--- data/tracker.R
+++ data/tracker.R
@@ -1,25 +1,27 @@
-install.packages("pdftools")
-install.packages("git2r")
+# run once
+# install.packages("pdftools")
+# install.packages("git2r")
+# install.packages("here")
 
 library(pdftools)
 library(git2r)
+library(here)
+
+repo <- repository()
+config(repo, user.name="zambujo", user.email="zambuj@gmail.com")
 
 # download latest Q&A and Factsheet
 download.file("https://www.sbfi.admin.ch/dam/sbfi/it/dokumente/2020/01/qa-h-europa.pdf.download.pdf/QA-Horizon-Europe_E.pdf", "QA-Horizon-Europe_E.pdf")
 download.file("https://www.sbfi.admin.ch/dam/sbfi/en/dokumente/2019/02/horizon-europe.pdf.download.pdf/Fact-sheet_Horizon_en.pdf", "Fact-sheet_Horizon_en.pdf")
 
-text = pdf_text("QA-Horizon-Europe_E.pdf")
-text = pdf_text("QA-Horizon-Europe_E.pdf")
-
-
 qa_text <- pdf_text("QA-Horizon-Europe_E.pdf")
-writeLines(qa_text, "QA-Horizon-Europe_E.txt")
+writeLines(qa_text, here("data", "QA-Horizon-Europe_E.txt"))
 
 factsheet_text <- pdf_text("Fact-sheet_Horizon_en.pdf")
-writeLines(factsheet_text, "Fact-sheet_Horizon_en.txt")
+writeLines(factsheet_text, here("data", "Fact-sheet_Horizon_en.txt"))
 
 # commiting changes
-add(path = "QA-Horizon-Europe_E.txt")
-add(path = "Fact-sheet_Horizon_en.txt")
+add(path = "data/QA-Horizon-Europe_E.txt")
+add(path = "data/Fact-sheet_Horizon_en.txt")
 
 commit(message = "Updated with R")
